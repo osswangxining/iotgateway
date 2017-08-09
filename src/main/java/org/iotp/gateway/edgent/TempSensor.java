808a4685a945ec1cd611baecf203cd8af9b90fd4
bjxnwang <bjxnwang@cn.ibm.com>
2017-08-09 16:17:25 +0800

    edge analytics

---
+++ src/main/java/org/iotp/gateway/edgent/TempSensor.java
@@ -0,0 +1,22 @@
+package org.iotp.gateway.edgent;
+
+import java.util.Random;
+
+import org.apache.edgent.function.Supplier;
+
+public class TempSensor implements Supplier<Double> {
+  double currentTemp = 65.0;
+  Random rand;
+
+  TempSensor() {
+    rand = new Random();
+  }
+
+  @Override
+  public Double get() {
+    // Change the current temperature some random amount
+    double newTemp = rand.nextGaussian() + currentTemp;
+    currentTemp = newTemp;
+    return currentTemp;
+  }
+}
---
+++ src/main/java/org/iotp/gateway/edgent/TempSensorApplication.java
@@ -0,0 +1,20 @@
+package org.iotp.gateway.edgent;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.edgent.providers.direct.DirectProvider;
+import org.apache.edgent.topology.TStream;
+import org.apache.edgent.topology.Topology;
+
+public class TempSensorApplication {
+  public static void main(String[] args) throws Exception {
+    TempSensor sensor = new TempSensor();
+    DirectProvider dp = new DirectProvider();
+    Topology topology = dp.newTopology();
+    TStream<Double> tempReadings = topology.poll(sensor, 100, TimeUnit.MILLISECONDS);
+    TStream<Double> filteredReadings = tempReadings.filter(reading -> reading < 50 || reading > 80);
+
+    filteredReadings.print();
+    dp.submit(topology);
+  }
+}
